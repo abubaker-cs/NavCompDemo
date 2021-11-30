0345feac69887b27f2e81852067909986d18825e
abubaker <mohammad_abubaker@yahoo.com>
2021-11-30 20:21:50 +0500

    Implementing code in the EnterDetailsFragment.kt to allow user to navigation to the VerifyDetailsFragment once the "Verify Details" button will be clicked.

--- app/src/main/java/org/abubaker/navcompdemo/EnterDetailsFragment.kt
+++ app/src/main/java/org/abubaker/navcompdemo/EnterDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class EnterDetailsFragment : Fragment() {
 
@@ -26,10 +27,12 @@ class EnterDetailsFragment : Fragment() {
         // Assign the click event to the Verify Details button.
         val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
 
+        // onClick Event for the Button: Verify Details
         btnVerifyDetails.setOnClickListener {
 
             // Navigate using findNavController and the action id where we want to navigate.
-            // findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment)
+            findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment)
+
         }
 
         return rootView
