009edbebe6f0260dc13edee10d0b8e5d96e1b059
abubaker <mohammad_abubaker@yahoo.com>
2021-12-01 00:56:48 +0500

    Bug fixed, now < button is working properly

--- app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
+++ app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
@@ -25,14 +25,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // The #fragment inside the activity_main contains our NavigationController
-        val navController = Navigation.findNavController(this, R.id.fragment)
+        navController = Navigation.findNavController(this@MainActivity, R.id.fragment)
         NavigationUI.setupActionBarWithNavController(this@MainActivity, navController)
 
     }
 
-    // Override the below method and pass the navController variable in it. After doing this the back button will be enable.
+    // Override the below method and
     override fun onSupportNavigateUp(): Boolean {
+
+        // Pass the navController variable in it, after doing this the back button will be enable.
         return NavigationUI.navigateUp(navController, null)
+
     }
 
 }
