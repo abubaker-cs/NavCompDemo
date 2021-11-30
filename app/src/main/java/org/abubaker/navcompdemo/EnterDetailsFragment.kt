d61c4a594b7a9c8aa34cb6073e9ba846df8d9149
abubaker <mohammad_abubaker@yahoo.com>
2021-11-30 12:45:10 +0500

    Working...

--- app/src/main/java/org/abubaker/navcompdemo/EnterDetailsFragment.kt
+++ app/src/main/java/org/abubaker/navcompdemo/EnterDetailsFragment.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EnterDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EnterDetailsFragment : Fragment() {
 
     /**
@@ -20,7 +16,22 @@ class EnterDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enter_details, container, false)
+        // return inflater.inflate(R.layout.fragment_enter_details, container, false)
+
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
+
+        // TODO Step 11: Assign the click event to the Verify Details button.
+        val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
+
+        btnVerifyDetails.setOnClickListener {
+
+            // TODO Step 12: Navigate using findNavController and the action id where we want to navigate.
+            // findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment)
+        }
+        return rootView
+
     }
 }
--- app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
+++ app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * onCreate()
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
