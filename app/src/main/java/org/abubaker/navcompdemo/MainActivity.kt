71a82a8bb12f837ed30cfb527e7fad2899abe00d
abubaker <mohammad_abubaker@yahoo.com>
2021-11-30 20:27:16 +0500

    Working...

--- app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
+++ app/src/main/java/org/abubaker/navcompdemo/MainActivity.kt
@@ -2,6 +2,8 @@ package org.abubaker.navcompdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // The #fragment inside the activity_main contains our NavigationController
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this@MainActivity, navController)
+
     }
 
 }
